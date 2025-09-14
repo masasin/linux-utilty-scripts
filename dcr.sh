fc4fc349a400bbfff4121791a6ac3403b2f723bd
Jean Nassar <jean@jnassar.com>
2025-09-14 03:38:00 +0200

    Fix name of docker compose

--- dcr.sh
+++ dcr.sh
@@ -11,10 +11,10 @@ service=$1
 shift
 
 # Check if the service container is running, without printing output (>/dev/null)
-if docker-compose ps -q "$service" | grep -q .; then
+if docker compose ps -q "$service" | grep -q .; then
   # If running, exec into it
-  docker-compose exec "$service" "$@"
+  docker compose exec "$service" "$@"
 else
   # If not running, run it with port mapping
-  docker-compose run --rm --service-ports "$service" "$@"
+  docker compose run --rm --service-ports "$service" "$@"
 fi
